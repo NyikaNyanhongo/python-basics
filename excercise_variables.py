92354322205ed0fe685db243c83b7995276a8ffe
Nyika Nyanhongo <nyika.nyanhongo@g.austincc.edu>
2025-07-16 12:51:07 -0500

    Completed excercise

--- excercise_variables.py
+++ excercise_variables.py
@@ -1,5 +1,7 @@
+#Qsn 1 
 onomatopoeia = "p"
 
+#Qsn 2 
 lyrics = '''Katie Casey was baseball mad,
 Had the fever and had it bad.
 Just to root for the home town crew,
@@ -11,37 +13,53 @@ To see a show, but Miss Kate said "No,
 I'll tell you what you can do:"'''
 #print(lyrics) test test
 
+#Qsn 3 
 # Create variable gpa, display gpa and display gpa variable type
 gpa = 3.75
 print(gpa)
 print(type(gpa))
 
-
+#Qsn 4
 # Create variable empty_var and assign None, display empty_var and display variable type
 empty_var = None
 print(empty_var)
 print(type(empty_var))
 
+#Qsn5
 # create exponent variable , assign value 828^282 , display variable
 exponent_calculation = 828**282
 print(exponent_calculation)
 
+#Qsn 6
 # create multiple_operations variable and display result of multiple ops expression 
 multiple_ops = (52-52) +(62/8) + (42%8 )
 print(multiple_ops)
 
+#Qsn 7
 #create boolean variable true set its value to True, create second variable isTrue and check that true is not true, display isTrue 
 true = True
 isTrue = true == True 
 print(isTrue)
 
+#Qsn 8 
 # create variable weird_world  set to Weird value, create 2nd value checking is weird_word matches weird
 weird_word = "Weird"
 weird_word_match_checker = weird_word == "weird"
 print(weird_word_match_checker)
 
+#Qsn 9
 #create variable 'false' and explain error
 false = "test"
  # Explanation: - There is no error when creating a variable 'false' with lowercase f but there would be a syntax error if uppercase F were used since Python is case sensitive 
 
- 
+#Qsn 10 
+##create variables for 35/7, 15^3, var2 % var1 and var2/var1, compare the results . (variable = var)
+var_1 = 35/7
+var_2 = 15**3
+var_3 = 2 % 1 
+var_4 = 2/1
+
+print(var_1,var_2, var_3,var_4)
+
+#comparing the results (was not sure how to compare so l compared the results by size )
+3375 > 5.0 > 2.0 > 0
