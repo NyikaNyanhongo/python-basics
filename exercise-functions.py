c136a7f02a31cf84e429a8983175f86b99b8f030
Nyika Nyanhongo <nyika.nyanhongo@g.austincc.edu>
2025-07-18 10:56:10 -0500

    commit #2 7-18-2025

--- exercise-functions.py
+++ exercise-functions.py
@@ -1,4 +1,27 @@
-def red_light(state):
-    print("Stop!, The light is red")
+# Defining the red light function
+def red_light():
+    print("Stop! The light is red.")
 
-red_light
+# Defining the yellow light function
+def yellow_light():
+    print("Caution! The light is yellow.")
+
+# Defining the green light function
+def green_light():
+    print("Go! The light is green.")
+
+
+def traffic_light(state):
+    if state == "red":
+        red_light()
+    elif state == "yellow":
+        yellow_light()
+    elif state == "green":
+        green_light()
+    else:
+         print("Please input red, yellow or green only.")
+
+traffic_light("green")
+traffic_light("red")
+traffic_light("yellow")
+traffic_light("focaccia green")       
