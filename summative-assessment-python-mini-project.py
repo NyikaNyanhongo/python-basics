909829ad9c0bda7787e93c474c17f6d5f9d05a1d
Nyika Nyanhongo <nyika.nyanhongo@g.austincc.edu>
2025-07-21 10:29:29 -0500

    summative-inprogress2

--- summative-assessment-python-mini-project.py
+++ summative-assessment-python-mini-project.py
@@ -6,7 +6,7 @@ def get_letter_grade(score):
         return 'A'
     elif score > 80: 
         return 'B'
-    elif score >70:
+    elif score > 70:
         return 'C'
     elif score > 60:
         return 'D'
@@ -16,5 +16,18 @@ def get_letter_grade(score):
 
 # 2) Define the function "print_summary(student_list)"- we need to display a list 
 #     of students showing the name, the scoe and the letter grade 
+
+def print_summary(student_list):
+    print("Student Information List: ")
+    for student in student_list:
+        print(f"{student['name']}: {student['score']} {student['grade']}")
+
 # 3) Define the function "save_to_file(student_list)" - we are using this to save 
 #     this info to a file called grades.txt IN WRITE MODE 'w'
+
+def save_to_file(student_list):
+    with open("geades.txt","w") as file:
+        for student in student_list:
+            file.write(f"{student['name']}: {student['score']} {student['grade']}")
+
+            
