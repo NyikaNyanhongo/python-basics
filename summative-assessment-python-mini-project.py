9698cfa5dfac26a8e8f15898db9cfc933a8d0eca
Nyika Nyanhongo <nyika.nyanhongo@g.austincc.edu>
2025-07-21 14:57:27 -0500

    summative-assessment-final-commit

--- summative-assessment-python-mini-project.py
+++ summative-assessment-python-mini-project.py
@@ -1,4 +1,4 @@
-# 1) Define the function "get_letter _grade(score)"- we are converting a
+'''# 1) Define the function "get_letter _grade(score)"- we are converting a
 #    number grade into a letter grade 
 
 def get_letter_grade(score):
@@ -137,4 +137,66 @@ print("\nStudent data saved to grades.txt")
 main()
 
 
-#Code broken, wont proceed after entering name?!!!! :-( ...fix after lunch
+#Code broken, wont proceed after entering name?!!!! :-( ...fix after lunch'''
+
+def get_letter_grade(score):
+    
+    if score >= 90:
+        return 'A'
+    elif score >= 80:
+        return 'B'
+    elif score >= 70:
+        return 'C'
+    elif score >= 60:
+        return 'D'
+    else:
+        return 'F'
+
+
+def print_summary(student_list):
+
+    print("\nStudent Summary:")
+    for student in student_list:
+        print(f"{student['name']}: {student['score']} -> {student['grade']}")
+
+
+def save_to_file(student_list):
+  
+    with open("grades.txt", "w") as file:
+        for student in student_list:
+            file.write(f"{student['name']}: {student['score']} -> {student['grade']}\n")
+
+
+def main():
+    print("Welcome to the Student Grade Tracker!\n")
+    students = []
+
+    while True:
+        name = input("Enter student name: ").strip().title()
+
+     
+        while True:
+            try:
+                score_input = input("Enter score (0–100): ")
+                score = float(score_input)
+                if 0 <= score <= 100:
+                    break
+                else:
+                    print("Please enter a score between 0 and 100.")
+            except ValueError:
+                print("Input not  Valid. Please enter a number....!")
+
+        grade = get_letter_grade(score)
+        students.append({"name": name, "score": score, "grade": grade})
+
+        cont = input("\nDo you want to add another student? (Y/N): ").strip().lower()
+        if cont != 'y':
+            break
+
+    print_summary(students)
+    save_to_file(students)
+    print("\nStudent data saved to grades.txt")
+
+
+
+main()
