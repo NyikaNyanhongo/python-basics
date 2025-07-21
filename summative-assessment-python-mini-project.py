97463ed000d73ac2a11026c083d861c3fd88169a
Nyika Nyanhongo <nyika.nyanhongo@g.austincc.edu>
2025-07-21 10:01:29 -0500

    summative-inprogress-commit

---
+++ summative-assessment-python-mini-project.py
@@ -0,0 +1,20 @@
+# 1) Define the function "get_letter _grade(score)"- we are converting a
+#    number grade into a letter grade 
+
+def get_letter_grade(score):
+    if score > 90:
+        return 'A'
+    elif score > 80: 
+        return 'B'
+    elif score >70:
+        return 'C'
+    elif score > 60:
+        return 'D'
+    else: 
+        return 'F'
+    
+
+# 2) Define the function "print_summary(student_list)"- we need to display a list 
+#     of students showing the name, the scoe and the letter grade 
+# 3) Define the function "save_to_file(student_list)" - we are using this to save 
+#     this info to a file called grades.txt IN WRITE MODE 'w'
